f2d0cff491dbbf36455b8bf9d19be7c0d0687a0a
Grashaar <grashaar@outlook.com>
2020-01-18 17:54:21 +0700

    Add TMP_NumberBinder, TMP_TextBinder

---
+++ MVVM/Binders/TMP_NumberBinder.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace UnuGames.MVVM
+{
+    [RequireComponent(typeof(TMP_Text))]
+    [DisallowMultipleComponent]
+    public class TMP_NumberBinder : BinderBase
+    {
+        protected TMP_Text text;
+
+        [HideInInspector]
+        public BindingField valueField = new BindingField("Number");
+
+        public string format;
+        public float timeChange = 0.25f;
+
+        public override void Initialize(bool forceInit)
+        {
+            if (!CheckInitialize(forceInit))
+                return;
+
+            this.text = GetComponent<TMP_Text>();
+            SubscribeOnChangedEvent(this.valueField, OnUpdateText);
+        }
+
+        public void OnUpdateText(object newNumber)
+        {
+            if (newNumber == null)
+                return;
+
+            double.TryParse(this.text.text, out var val);
+            double.TryParse(newNumber.ToString(), out var change);
+
+            UITweener.Value(this.gameObject, this.timeChange, (float)val, (float)change)
+                     .SetOnUpdate(OnUpdate)
+                     .SetOnComplete(() => OnComplete(newNumber));
+        }
+
+        private void OnUpdate(float val)
+        {
+            if (string.IsNullOrEmpty(this.format))
+            {
+                this.text.text = val.ToString();
+            }
+            else
+            {
+                this.text.text = string.Format(this.format, val);
+            }
+        }
+
+        private void OnComplete(object newNumber)
+        {
+            var text = newNumber.ToString();
+
+            if (string.IsNullOrEmpty(this.format))
+            {
+                this.text.text = text;
+            }
+            else
+            {
+                this.text.text = string.Format(this.format, text);
+            }
+        }
+    }
+}
---
+++ MVVM/Binders/TMP_TextBinder.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace UnuGames.MVVM
+{
+    [RequireComponent(typeof(TMP_Text))]
+    [DisallowMultipleComponent]
+    public class TMP_TextBinder : BinderBase
+    {
+        protected TMP_Text text;
+
+        [HideInInspector]
+        public BindingField textField = new BindingField("Text");
+
+        [HideInInspector]
+        public BindingField colorField = new BindingField("Color", true);
+
+        public string format;
+
+        public override void Initialize(bool forceInit)
+        {
+            if (!CheckInitialize(forceInit))
+                return;
+
+            this.text = GetComponent<TMP_Text>();
+
+            SubscribeOnChangedEvent(this.textField, OnUpdateText);
+            SubscribeOnChangedEvent(this.colorField, OnUpdateColor);
+        }
+
+        public void OnUpdateText(object val)
+        {
+            var newText = val == null ? string.Empty : val.ToString();
+
+            if (string.IsNullOrEmpty(this.format))
+            {
+                this.text.text = newText;
+            }
+            else
+            {
+                this.text.text = string.Format(this.format, newText);
+            }
+        }
+
+        public void OnUpdateColor(object val)
+        {
+            if (val == null)
+                return;
+
+            if (!(val is Color valChange))
+                return;
+
+            this.text.color = valChange;
+        }
+    }
+}
